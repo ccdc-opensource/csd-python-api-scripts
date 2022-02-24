d70cdaa53ae92a87b1d996e62d0269304c954d60
Alex Moldovan <amoldovan@ccdc.cam.ac.uk>
2022-02-24 12:25:27 +0000

    Added example script with boilerplate NO_JIRA

---
+++ scripts/new_script_readme_template/script_example.py
@@ -0,0 +1,9 @@
+#
+# This script can be used for any purpose without limitation subject to the
+# conditions at http://www.ccdc.cam.ac.uk/Community/Pages/Licences/v2.aspx
+#
+# This permission notice and the following statement of attribution must be
+# included in all copies or substantial portions of this script.
+#
+# 0000-00-00: created by Name, Institute
+#
