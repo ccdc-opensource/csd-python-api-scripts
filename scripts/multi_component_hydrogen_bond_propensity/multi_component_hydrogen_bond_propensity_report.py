8cf0f0e1b086bf933b00afb35367bfa0d695927b
Joanna <joanna.stevens@ccdc.cam.ac.uk>
2022-06-22 13:22:17 +0100

    Allows report generation if calc failures NO_JIRA

--- scripts/multi_component_hydrogen_bond_propensity/multi_component_hydrogen_bond_propensity_report.py
+++ scripts/multi_component_hydrogen_bond_propensity/multi_component_hydrogen_bond_propensity_report.py
@@ -355,7 +355,7 @@ def main(structure, work_directory, library, csdrefcode):
             mc_dictionary[coformer_name] = ["N/A", "N/A", "N/A", "N/A", "N/A", crystal.identifier]
 
     # Make sense of the outputs of all the calculations
-    mc_hbp_screen = sorted(mc_dictionary.items(), key=lambda e: e[1][0], reverse=True)
+    mc_hbp_screen = sorted(mc_dictionary.items(), key=lambda e: 0 if e[1][0] == 'N/A' else e[1][0], reverse=True)
     diagram_file = make_diagram(api_molecule, work_directory)
     chart_file = make_mc_chart(mc_hbp_screen, directory, api_molecule)
     make_mc_report(structure, mc_hbp_screen, work_directory, diagram_file, chart_file)
